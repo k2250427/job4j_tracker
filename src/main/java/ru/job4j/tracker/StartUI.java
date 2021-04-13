b1ae2265529af0e77c964674aa5d83500ff4794f
Stasyuk.K <kstasyuk@ya.ru>
2021-04-13 17:31:02 +0300

    2.2. Реализация класса StartUI. Вывод меню [#512806]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -10,78 +10,26 @@ public class StartUI {
             this.showMenu();
             System.out.print("Select: ");
             int select = Integer.valueOf(scanner.nextLine());
-            if (select == 0) {
-                System.out.println("=== Create a new Item ====");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item item = new Item(name);
-                tracker.add(item);
-            } else if (select == 1) {
-                System.out.println("=== List of Items ====");
-                Item[] list = tracker.findAll();
-                for (Item n: list) {
-                    System.out.println(n.toString());
-                }
-            } else if (select == 2) {
-                System.out.println("=== Edit a selected Item ====");
-                System.out.print("Enter id: ");
-                int id = Integer.valueOf(scanner.nextLine());
-                Item item = tracker.findById(id);
-                if (item != null) {
-                    Item newItem = new Item(item.getName());
-                    if (tracker.replace(id, newItem)) {
-                        System.out.println("Item saved");
-                    } else {
-                        System.out.println("Item saving failed");
-                    }
-                }
-            } else if (select == 3) {
-                System.out.println("=== Delete selected Item ====");
-                System.out.print("Enter id: ");
-                int id = Integer.valueOf(scanner.nextLine());
-                if (tracker.delete(id)) {
-                    System.out.println("Item deleted");
-                } else {
-                    System.out.println("Item deletion failed");
-                }
-            } else if (select == 4) {
-                System.out.println("=== Select Item by id ====");
-                System.out.print("Enter id: ");
-                int id = Integer.valueOf(scanner.nextLine());
-                Item item = tracker.findById(id);
-                if (item != null) {
-                    System.out.println(item.toString());
-                } else {
-                    System.out.println("Item not found");
-                }
-            } else if (select == 5) {
-                System.out.println("=== Find Items by name ====");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
-                Item[] list = tracker.findByName(name);
-                if (list.length > 0) {
-                    for (Item n: list) {
-                        System.out.println(n.getName());
-                    }
-                } else {
-                    System.out.println("Item not found");
-                }
-            } else if (select == 6) {
+            if (select != 6) {
+                System.out.println("Пользователь выбрал: " + select);
+            } else {
                 run = false;
             }
         }
     }
 
     private void showMenu() {
-        System.out.println("Menu.");
-        System.out.println("0. Add new Item");
-        System.out.println("1. Show all items");
-        System.out.println("2. Edit item");
-        System.out.println("3. Delete item");
-        System.out.println("4. Find item by Id");
-        System.out.println("5. Find items by name");
-        System.out.println("6. Exit Program");
+        String[] menu = {
+                "Add new Item", "Show all items", "Edit item",
+                "Delete item", "Find item by id", "Find items by name",
+                "Exit Program"
+        };
+        System.out.println("Menu:");
+        for (int i = 0; i < menu.length; i++) {
+            System.out.println(i + ". " + menu[i]);
+        }
     }
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
