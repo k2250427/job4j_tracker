966e41c1105932fff13d5cc7b1a13c925a0533a5
Stasyuk.K <kstasyuk@ya.ru>
2021-04-14 12:18:01 +0300

    4.1. Разрыв зависимости StartUI от Scanner. [#512801]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -72,7 +72,7 @@ public class StartUI {
         boolean run = true;
         while (run) {
             this.showMenu();
-            int select = Integer.valueOf(input.askStr("Select: "));
+            int select = Integer.parseInt(input.askStr("Select: "));
             if (select == 0) {
                 StartUI.createItem(input, tracker);
             }  else if (select == 1) {
