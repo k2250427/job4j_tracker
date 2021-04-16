e87d66f1af826707e8c673f3dfee58e8727b99bc
Stasyuk.K <kstasyuk@ya.ru>
2021-04-16 17:17:35 +0300

    2. Singleton [#512838]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,40 +1,40 @@
 package ru.job4j.tracker;
 
 public class SingleTracker {
-    private static Tracker tracker = new Tracker();
+    private static SingleTracker instance  = null;
 
     private SingleTracker() {
 
     }
 
-    public static Tracker getInstance() {
-        if (tracker == null) {
-            tracker = new Tracker();
+    public static SingleTracker getInstance() {
+        if (instance == null) {
+            instance = new SingleTracker();
         }
-        return tracker;
+        return instance;
     }
 
     public Item add(Item item) {
-        return tracker.add(item);
+        return instance.add(item);
     }
 
     public boolean replace(int id, Item item) {
-        return tracker.replace(id, item);
+        return instance.replace(id, item);
     }
 
     public boolean delete(int id) {
-        return tracker.delete(id);
+        return instance.delete(id);
     }
 
     public Item findById(int id) {
-        return tracker.findById(id);
+        return instance.findById(id);
     }
 
     public Item[] findAll() {
-        return tracker.findAll();
+        return instance.findAll();
     }
 
     public Item[] findByName(String key) {
-        return tracker.findByName(key);
+        return instance.findByName(key);
     }
 }
