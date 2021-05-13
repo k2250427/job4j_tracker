34bed828c72b14bfe4b8d7d9cb1252c0e7abe6f2
Stasyuk.K <kstasyuk@ya.ru>
2021-05-13 09:37:04 +0300

    1. Генератор текста

--- src/main/java/ru/job4j/collection/FreezeStr.java
+++ src/main/java/ru/job4j/collection/FreezeStr.java
@@ -1,13 +1,29 @@
 package ru.job4j.collection;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class FreezeStr {
     public static boolean eq(String left, String right) {
-        char[] leftArray = left.toCharArray();
-        Arrays.sort(leftArray);
-        char[] rightArray = right.toCharArray();
-        Arrays.sort(rightArray);
-        return Arrays.equals(leftArray, rightArray);
+        HashMap<Character, Integer> leftMap = buildMap(left);
+        HashMap<Character, Integer> rightMap = buildMap(right);
+        for (Character ch: rightMap.keySet()) {
+            if(leftMap.get(ch) == null || leftMap.get(ch) < leftMap.get(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static HashMap<Character, Integer> buildMap(String origin) {
+        char[] originChars = origin.toLowerCase().toCharArray();
+        HashMap<Character, Integer> rsl = new HashMap<>();
+        for (char ch: originChars) {
+            if(rsl.containsKey(ch)) {
+                rsl.replace(ch, rsl.get(ch) + 1);
+            } else {
+                rsl.put(ch, 1);
+            }
+        }
+        return rsl;
     }
 }
