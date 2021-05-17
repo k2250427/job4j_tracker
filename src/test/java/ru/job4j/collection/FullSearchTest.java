42561ab24ee2d275bbe83082fdb24cfd3532ccf1
Stasyuk.K <kstasyuk@ya.ru>
2021-05-17 17:34:51 +0300

    1. Метод .of [#512617]

--- src/test/java/ru/job4j/collection/FullSearchTest.java
+++ src/test/java/ru/job4j/collection/FullSearchTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
-import java.util.Arrays;
+
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -13,12 +13,12 @@ public class FullSearchTest {
 
     @Test
     public void extractNumber() {
-        List<Task> tasks = Arrays.asList(
+        List<Task> tasks = List.of(
                 new Task("1", "First desc"),
                 new Task("2", "Second desc"),
                 new Task("1", "First desc")
         );
-        Set<String> expected = new HashSet<>(Arrays.asList("1", "2"));
+        Set<String> expected = new HashSet<>(List.of("1", "2"));
         assertThat(FullSearch.extractNumber(tasks), is(expected));
     }
 }
