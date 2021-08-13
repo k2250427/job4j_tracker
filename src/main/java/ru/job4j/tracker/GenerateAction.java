784dcd08613d2778c131c0cef7f8bc730dc0fc9c
Stasyuk.K <kstasyuk@ya.ru>
2021-08-13 17:14:10 +0300

    1. Эксперименты с различными GC [#1590 #189203]

--- src/main/java/ru/job4j/tracker/GenerateAction.java
+++ src/main/java/ru/job4j/tracker/GenerateAction.java
@@ -9,13 +9,13 @@ public class GenerateAction implements UserAction {
 
     @Override
     public String name() {
-        return "Add 100k new Items";
+        return "Add 50k new Items";
     }
 
     @Override
     public boolean execute(Input input, Store memTracker) {
-        out.println("=== Create a 100k new Items ====");
-        for (int i = 0; i < 100000; i++) {
+        out.println("=== Create a 50k new Items ====");
+        for (int i = 0; i < 50000; i++) {
             Item item = new Item("Item ".concat(Integer.toString(i)));
             memTracker.add(item);
         }
